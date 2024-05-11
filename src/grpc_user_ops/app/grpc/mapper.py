d3c682ce65226db390e2ca646d35dfdb58f3dfbd
BadrTarek <badr.tarek20@gmail.com>
2024-05-11 15:07:49 +0300

    create mapper to map user dict to user protos

---
+++ src/grpc_user_ops/app/grpc/mapper.py
@@ -0,0 +1,19 @@
+
+from grpc_user_ops.protos_models.user_pb2 import (
+    User
+)
+from google.protobuf.json_format import ParseDict
+from google.protobuf.timestamp_pb2 import Timestamp
+
+
+
+
+
+def map_user_entity_to_user_protos(user_dict:dict) -> User:
+    user_dict['id'] = str(user_dict.get('id'))
+    created_at = Timestamp()
+    created_at.FromDatetime(user_dict['created_at'])
+    user_dict['created_at'] = created_at.ToJsonString()
+    user = User()
+    ParseDict(user_dict, user)
+    return user
