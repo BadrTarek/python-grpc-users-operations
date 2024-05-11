e3d677cc6171ffe8fddffba24737cf65fce47f26
BadrTarek <badr.tarek20@gmail.com>
2024-05-12 01:08:06 +0300

    add logging config to data settings

--- src/grpc_user_ops/config/data_settings.py
+++ src/grpc_user_ops/config/data_settings.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv,find_dotenv
+import logging
+
 
 load_dotenv(find_dotenv(".env",raise_error_if_not_found=False))
 
@@ -18,3 +20,13 @@ MAX_WORKERS = int(os.environ.get("MAX_WORKERS","10"))
 GRPC_SERVER_PORT = int(os.environ.get("GRPC_SERVER_PORT","50051"))
 SERVE_INSECURE = bool(os.environ.get("SERVE_INSECURE", "1"))
 
+# Logging Configuration
+__LOG_LEVELS_IN_STR = {
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL
+}
+LOGGING_LEVEL = __LOG_LEVELS_IN_STR.get(os.environ.get("LOGGING_LEVEL","DEBUG"),logging.DEBUG)
+LOGGING_FILE = os.environ.get("LOGGING_FILE","grpc-user-ops.log")
