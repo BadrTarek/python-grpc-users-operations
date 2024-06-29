4fd8fce929cbb8e6135fc6dd26f2ed70bdb98768
BadrTarek <badr.tarek20@gmail.com>
2024-06-29 17:22:16 +0300

    add integration test for delete user rpc

---
+++ tests/integration_tests/grpc/test_delete_user_rpc.py
@@ -0,0 +1,65 @@
+import grpc
+from grpc_user_ops.data.database.sqlalchemy_unit_of_work import SqlAlchemyUnitOfWork
+from protobufs.user_ops_api_pb2_grpc import  UserOpsApiStub
+from grpc_user_ops.config import data_settings
+import unittest
+from protobufs.user_ops_api_pb2 import (
+    DeleteUserRequest,
+    Empty
+)
+from grpc_user_ops.data.logger.default_logger import DefaultLogger
+import pytest
+from tests.integration_tests.test_helper import generate_grpc_test_server   
+from grpc_user_ops.domain.entities.user import User as UserEntity
+
+
+class RPCDeleteUserTest(unittest.IsolatedAsyncioTestCase):
+    
+    def setUp(self):
+        self.server = generate_grpc_test_server()
+        
+        self.logger = DefaultLogger()
+        
+        self.uow = SqlAlchemyUnitOfWork(self.logger)
+        
+        self.server.start()
+
+
+    def tearDown(self):
+        self.server.stop(None)
+
+
+    @pytest.mark.asyncio
+    async def test_success_delete_user(self):
+        created_user_in_db = None
+        
+        async with self.uow:
+            created_user_in_db = await self.uow.user_repository.create(
+                UserEntity.model_construct(
+                    name = "Test",
+                    email = "test@mail.com",
+                    phone = "+2011111111"
+                )
+            )
+            await self.uow.commit()
+        
+        deleted_user_protos = DeleteUserRequest(
+            id = str(created_user_in_db.id),
+        )
+        
+        with grpc.insecure_channel(f'localhost:{data_settings.GRPC_SERVER_PORT}') as channel:
+            stub = UserOpsApiStub(channel)
+            
+            response = stub.DeleteUser(
+                DeleteUserRequest(
+                    id = str(created_user_in_db.id),
+                )
+            )
+            
+            self.assertIsInstance(response,Empty)
+        
+        
+        
+        async with self.uow:
+            deleted_user_entity = await self.uow.user_repository.get(deleted_user_protos.id)
+            self.assertIsNone(deleted_user_entity)
